f319c7d1e7f0fc817a03aecb201e617490c3b3fe
Otubusin Ademuyiwa (Innovate. Educate. Elevate.) <53128146+tay4real@users.noreply.github.com>
2025-09-12 12:26:11 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // const http = require('http');
 const express = require('express');
 const app = express();
-const cors = require('cors');
 
-app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'));
 
