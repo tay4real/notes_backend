0f6a84d2c0f938a26824303c84cb46d3405a4509
Otubusin Ademuyiwa (Innovate. Educate. Elevate.) <53128146+tay4real@users.noreply.github.com>
2025-09-19 15:29:51 +0100

    update

--- index.js
+++ index.js
@@ -62,10 +62,9 @@ app.get('/api/notes/:id', (request, response) => {
 });
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
 });
 
 const generateId = () => {
@@ -79,20 +78,17 @@ app.post('/api/notes', (request, response) => {
   const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({
-      error: 'Content missing',
-    });
+    return response.status(400).json({ error: 'content missing' });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
